6e36a295957b321c888af7db9eac24009679702c
bhagavan2010 <39654595+bhagavan2010@users.noreply.github.com>
2018-12-26 21:44:45 +0530

    Add just line

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "Hello world! Bhagavan here its a Good sign for world and good for health";
+        return "Hello world! Bhagavan here its a Good sign for world and good for health and fine";
     }
 }
