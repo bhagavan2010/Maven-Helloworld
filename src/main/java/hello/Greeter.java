57936b62d2240d843a97366700595b24a2e1b944
Reddy <bhagavan.gouru@atos.net>
2018-12-26 20:47:15 +0530

    adding line in greeter java file

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "Hello world! Bhagavan here its a Good sign";
+        return "Hello world! Bhagavan here its a Good sign for world";
     }
 }
