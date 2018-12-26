c92462301a7c2a7115c47b03ea9a3818c09688ae
bhagavan2010 <39654595+bhagavan2010@users.noreply.github.com>
2018-12-26 20:37:34 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "Hello world! Bhagavan here";
+        return "Hello world! Bhagavan here its a Good sign";
     }
 }
